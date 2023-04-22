d5e1da8e3aaadcd46ae1a4ff7a613aa3662aa467
EC2 Default User <ec2-user@ip-172-31-4-28.ap-northeast-1.compute.internal>
2023-04-22 06:00:03 +0000

    aboutpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
+  get 'home/about'=>'homes#about'
 
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
