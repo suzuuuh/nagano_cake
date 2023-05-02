ef38b0482dd52ba19f3696c782c2721683e9c58c
EC2 Default User <ec2-user@ip-172-31-4-28.ap-northeast-1.compute.internal>
2023-05-02 10:46:02 +0000

    会員一覧

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -12,5 +12,12 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "You have created book successfully."
+    redirect_to admin_customer_path(@customer.id)
+    else
+      render :edit
+    end
   end
 end
